6f43b97099a9800f7f427d9c5e984f7b4bcd0eca
Richard Skogeby <richard.skogeby@outlook.com>
2019-01-05 15:17:21 +0000

    Part 1 finished, infinite list of two word compound startup names

--- lib/main.dart
+++ lib/main.dart
@@ -7,15 +7,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          child: RandomWords(),
-        ),
-      ),
+      title: 'Startup Name Generator',
+      home: RandomWords(),
     );
   }
 }
@@ -25,22 +18,28 @@ class RandomWordsState extends State<RandomWords> {
   final _biggerFont = const TextStyle(fontSize: 18.0);
 
   Widget build(BuildContext context) {
-    final wordpair = WordPair.random();
-    return Text(wordpair.asPascalCase);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
   }
+
   Widget _buildSuggestions() {
     return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder:  (context, i) {
-        if (i.isOdd) return Divider(); 
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return Divider();
 
-        final index = i ~/ 2; 
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10)); 
-        }
-        return _buildRow(_suggestions[index]);
-      });
+          final index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
   }
+
   Widget _buildRow(WordPair pair) {
     return ListTile(
       title: Text(
@@ -48,11 +47,10 @@ class RandomWordsState extends State<RandomWords> {
         style: _biggerFont,
       ),
     );
-  } 
+  }
 }
 
 class RandomWords extends StatefulWidget {
   @override
   RandomWordsState createState() => new RandomWordsState();
 }
-
